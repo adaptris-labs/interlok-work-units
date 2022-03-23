79273d7aef186b1fc243a9774876528a496a8825
Sebastien Belin <sebastien.belin@adaptris.com>
2022-03-23 06:54:01 +0000

    Modify the work unit service so we can avoid conflict when several work unit have the same xml file name.
    The work unit name property now refer to the jar name and an xml config file name property can be specified in case it differs from the default value.

--- src/main/java/com/adaptris/workunit/WorkUnitURLStreamHandlerProvider.java
+++ src/main/java/com/adaptris/workunit/WorkUnitURLStreamHandlerProvider.java
@@ -6,6 +6,10 @@ import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.net.spi.URLStreamHandlerProvider;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.adaptris.workunit.util.WorkUnitDetector;
+
 public class WorkUnitURLStreamHandlerProvider extends URLStreamHandlerProvider {
 
   private static final String WORK_UNIT_PROTOCOL = "workunit";
@@ -16,11 +20,20 @@ public class WorkUnitURLStreamHandlerProvider extends URLStreamHandlerProvider {
       return new URLStreamHandler() {
         @Override
         protected URLConnection openConnection(URL url) throws IOException {
-          return ClassLoader.getSystemClassLoader().getResource(url.getPath()).openConnection();
+          return convertUrl(url).openConnection();
         }
       };
     }
     return null;
+  }
+
+  protected URL convertUrl(URL url) throws IOException {
+    String[] split = url.getPath().split("!/");
+
+    final String jarName = split.length > 1 ? split[0] : null;
+    String path = url.getPath().replaceFirst(StringUtils.trimToEmpty(jarName) + "!/", "");
+
+    return WorkUnitDetector.listUrls(path).stream().filter(u -> WorkUnitDetector.isCorrectJar(u, jarName)).findFirst().get();
   }
 
 }
--- src/main/java/com/adaptris/workunit/services/WorkUnitService.java
+++ src/main/java/com/adaptris/workunit/services/WorkUnitService.java
@@ -1,11 +1,18 @@
-package com.adaptris.workunit.service;
+package com.adaptris.workunit.services;
 
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.validation.constraints.NotBlank;
+
+import org.apache.commons.lang3.StringUtils;
+
 import com.adaptris.annotation.AdapterComponent;
+import com.adaptris.annotation.AdvancedConfig;
 import com.adaptris.annotation.ComponentProfile;
 import com.adaptris.annotation.DisplayOrder;
+import com.adaptris.annotation.InputFieldDefault;
+import com.adaptris.annotation.InputFieldHint;
 import com.adaptris.core.AdaptrisMarshaller;
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.CoreException;
@@ -19,6 +26,7 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @XStreamAlias("work-unit-service")
@@ -28,17 +36,40 @@ import lombok.Setter;
 public class WorkUnitService extends ServiceImp {
 
   private static final String WORK_UNIT_URL_PREFIX = "workunit:";
-  
+
   private static final String XML_EXTENSION = ".xml";
-  
+  private static final String DEFAULY_XML_NAME = "work-unit";
+  private static final String SEPARATOR = "!/";
+
+  /**
+   * The name of the work unit jar file without the .jar
+   *
+   * @param workUnitName
+   * @return String workUnitName
+   */
+  @NotBlank
+  @InputFieldHint(style = "com.adaptris.workunit.util.WorkUnitDetector#list")
   @Getter
   @Setter
+  @NonNull
   private String workUnitName;
-  
+
+  /**
+   * The name of the work unit config xml file without the .xml By default 'work-unit' is used
+   *
+   * @param xmlConfigName
+   * @return String xmlConfigName
+   */
+  @AdvancedConfig
+  @InputFieldDefault(DEFAULY_XML_NAME)
+  @Getter
+  @Setter
+  private String xmlConfigName;
+
   @Getter(value = AccessLevel.PROTECTED)
   @Setter(value = AccessLevel.PROTECTED)
   private Service proxiedService;
-  
+
   @Override
   public void doService(AdaptrisMessage msg) throws ServiceException {
     getProxiedService().doService(msg);
@@ -48,26 +79,30 @@ public class WorkUnitService extends ServiceImp {
     AdaptrisMarshaller marshaller = DefaultMarshaller.getDefaultMarshaller();
     return (Service) marshaller.unmarshal(inputStream);
   }
-  
+
   @Override
   public void prepare() throws CoreException {
     try {
       setProxiedService(
           deserializeService(
-              new URL(WORK_UNIT_URL_PREFIX + getWorkUnitName() + XML_EXTENSION).openStream()
-          )
-      );
+              new URL(WORK_UNIT_URL_PREFIX + getWorkUnitName() + SEPARATOR + xmlConfigName() + XML_EXTENSION).openStream()
+              )
+          );
       LifecycleHelper.prepare(getProxiedService());
     } catch (Exception e) {
       throw ExceptionHelper.wrapCoreException(e);
-    } 
+    }
+  }
+
+  private String xmlConfigName() {
+    return StringUtils.defaultIfBlank(DEFAULY_XML_NAME, getXmlConfigName());
   }
 
   @Override
   public void start() throws CoreException {
     LifecycleHelper.start(getProxiedService());
   }
-  
+
   @Override
   public void stop() {
     LifecycleHelper.stop(getProxiedService());
